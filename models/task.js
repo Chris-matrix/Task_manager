0ec059b57ded6bf32fa8dd3df82de5cd3ece84cb
Chris-matrix <cwatt0046@launchapd.org>
2024-12-12 17:26:11 -0500

    update

--- models/task.js
+++ models/task.js
@@ -1,23 +1,25 @@
-import { DataTypes } from "sequelize";
-import sequlize from "../config/dbconn.js";
+import { DataTypes } from 'sequelize';
+import sequelizeConnection from '../config/config.js';
 
-// add comments here
-
-        const Task = sequlize.define("Task", {
-            title: {
-                type: DataTypes.STRING,
-                allowNull: false,
-        },
-        description: {
-            type: DataTypes.TEXT,
-        },
-        dueDate: {
-            type: DataTypes.DATE,
-        },
-        completed: {
-            type: DataTypes.BOOLEAN,
-            defaultValue: false
-        }
-    });
+const Task = sequelizeConnection.define('Task', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    description: {
+        type: DataTypes.STRING,
+    },
+    status: {
+        type: DataTypes.STRING,
+        defaultValue: 'pending',
+    },
+}, {
+    timestamps: true, // Adds createdAt and updatedAt columns
+});
 
 export default Task;
