b612e84770f37798437148ea6ad0c045e0b723a5
Chris-matrix <cwatt0046@launchapd.org>
2024-12-12 09:19:20 -0500

    message

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import taskRoutes from './routes/tasks.js';
 
 const app = express();
 
+app.use(express.static('public'));
 app.use(express.json());
 
 app.use("veiws engine", 'ejs');
@@ -11,7 +12,6 @@ app.use("veiws ","/veiws");
 
 app.use('/task', taskRoutes);
 
-app.use(express.static('public'));
 
 
 const port = process.env.PORT ||3000;
